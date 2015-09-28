3d54456a1d379f892f512fe8185c7c73ddc0d7a8
Pau5erra <pausc1@TR12-S03-21.tr12.cat>
2015-09-28 17:06:07 +0100

    asdfasdf

--- ProgII/ProgII/Source.cpp
+++ ProgII/ProgII/Source.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+struct llista{
 
+	char a[100];
+
+};
+
+void my_func(int* num){
+
+	*num = 15;
+}
 int main(){
+	int a=1;
 
-
-
+	my_func(&a);
+	
+	printf("%d", a);
+	getchar();
 	return 0;
 }

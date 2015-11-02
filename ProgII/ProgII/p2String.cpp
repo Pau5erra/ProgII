f826eb61f03367c2bc92474a2b6659169063ceb5
Pau5erra <pau.serra11@hotmail.com>
2015-11-02 14:44:45 +0100

    String.h

--- ProgII/ProgII/p2String.cpp
+++ ProgII/ProgII/p2String.cpp
@@ -1,11 +1,17 @@
 #include "p2String.h"
 #include "string.h"
 
+p2String::p2String(){
+	capacity = 0;
+	str = new char[capacity];
+	str = '\0';
+}
 
 p2String::p2String(const p2String& string){ 
 
-	str = string.str; 
-	capacity = string.capacity; 
+	capacity = strlen(string.c_str());
+	str = new char[capacity + 1];
+	strcpy_s(str, capacity, string.str);
 
 }
 
@@ -13,7 +19,7 @@ p2String::p2String(const char* string){
 
 	capacity = strlen(string);
 	str = new char[capacity+1];
-	strcpy(str, string);
+	strcpy_s(str, capacity, string);
 
 }
 
@@ -21,6 +27,10 @@ p2String::p2String(uint capacity){
 
 	this->capacity = capacity; 
 
+}
+
+p2String::~p2String(){
+	delete[] str;
 }
 
 const char* p2String::c_str() const{
@@ -35,10 +45,51 @@ uint p2String::GetCapacity(const char* str) const{
 
 }
 
-bool p2String::operator==(const p2String& string) const {
-	return true;
+bool p2String::operator==(const char* string) const{
+
+	if (strcmp(str, string) == 0){
+		return true;
+	}
+	return false;
 }
 
+bool p2String::operator==(const p2String& string) const {
+
+	if (strcmp(str, string.str) == 0){
+		
+		return true;
+	}
+
+	return false;
+}
+
+bool p2String::operator!=(const char* string) const{
+	if (strcmp(str, string) != 0){
+		return true;
+	}
+	return false;
+
+}
 bool p2String::operator!=(const p2String& string) const {
-	return true;
+
+	if (strcmp(str, string.str) != 0){
+		return true;
+	}
+	return false;
+}
+
+const p2String p2String::operator=(const char* string){
+	for (int i = 0; i == strlen(string); i++){
+
+		str[i] = string[i];
+	}
+
+	return *this;
+}
+
+const p2String p2String::operator=(const p2String &string){
+
+
+
+	return *this;
 }
--- ProgII/ProgII/p2String.h
+++ ProgII/ProgII/p2String.h
@@ -1,7 +1,8 @@
 #ifndef _P2STRING_
 #define _P2STRING_
 
-#include <iostream>
+
+typedef unsigned int uint;
 
 class p2String{
 	//LA capacitat no és el tamany de la cadena, puc tenir una cadena de 2 i una capacitat de 2000000000
@@ -10,29 +11,38 @@ class p2String{
 	//Si estan privats:
 	//Puc controlar memòria
 private:
+
 	char* str;
 	uint capacity;
 
 public:
-	p2String(){};
+	p2String();
+	//constructor copia
 	p2String(const p2String& string);
+	//constructor c string
 	p2String(const char* string);
 	p2String(uint capacity);
 
-	~p2String(){};
+	~p2String();
 
 	//getter
 	const char* c_str() const;
 	uint GetCapacity(const char* str) const;
 
 	//Operadors
+	bool operator== (const char* string) const;
 	bool operator==(const p2String& string) const;
+	bool operator!= (const char* string) const;
 	bool operator!=(const p2String& string) const;
+
+	//assignació
+	const p2String operator= (const char* string);
+	const p2String operator= (const p2String& string);
 
 	//Fer l'operador d'assignació. Comprovar si hi ha memòria. Si n'hi ha suficient copies
 	//Si no n'hi ha suficient crear més memòria (new)
 };
 
-typedef unsigned int uint;
+
 
 #endif

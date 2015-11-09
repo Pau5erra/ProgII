a906d539447288f75150d7d7bff00202aa403b33
Pau5erra <pausc1@TR12-S03-21.tr12.cat>
2015-11-09 17:11:58 +0100

    Dyn array correcte

--- ProgII/ProgII/Definitions.h
+++ ProgII/ProgII/Definitions.h
@@ -1,6 +1,10 @@
-#define BLOCK 20
+#ifndef _DEFINITIONS_
+#define _DEFINITIONS_
+
 
 #define MIN(a,b)  ((a)<(b)) ? a : b
 #define MAX(a,b)  ((a)>(b)) ? a : b
 
 typedef unsigned int uint;
+
+#endif
--- ProgII/ProgII/DynArray.h
+++ ProgII/ProgII/DynArray.h
@@ -2,28 +2,39 @@
 #define _DYNARRAY_
 #include "Definitions.h"
 
-template <class tmp>
+#define BLOCK 20
+template <class TYPE>
 
 class DynArray{
 private:
-	uint capacity= 0;
+	uint capacity= BLOCK;
 	uint num_elem=0;
-	tmp* data=NULL;
+	TYPE* data=NULL;
 
 public:
 	DynArray():capacity(BLOCK){  //Aqui a capacity li poso BLOCK i al fer aixó ja no el posa a 0
 
-		data = new tmp[BLOCK];
+		data = new TYPE[BLOCK];
 
 	}
 
 	DynArray(const DynArray& new_array): num_elem(new_array.num_elem){
 
-		capacity = MAX(BLOCK, new_array.capacity);
-		data = new tmp[capacity];
-		memcpy(data, new_array.data, new_array.num_elem*sizeof(tmp));
+		capacity = MAX(BLOCK, new_array.num_elem);
+		data = new TYPE[capacity];
+		memcpy(data, new_array.data, new_array.num_elem*sizeof(TYPE));
 	}
+	
+	//constructor reserva de memòria
+	DynArray(uint new_capacity){//NO es pot fer const de uint
+		if (capacity < new_capacity){
 
+			capacity = new_capacity;
+
+		}
+
+		data = new TYPE[capacity];
+	}
 
 	~DynArray(){
 		if (data != NULL){
@@ -32,10 +43,18 @@ public:
 	}
 
 	
-	void PushBack(const tmp& new_element){
-		
-		if(capacity>=num_elem++)
-		data[num_elem] = new_element;
+	void PushBack(const TYPE& new_element){
+
+		if (capacity < num_elem){
+
+			TYPE* tmp = new tmp[capacity];
+			data = new TYPE[capacity + BLOCK];
+			memcpy(data, tmp, num_elem*sizeof(TYPE);
+			delete[] tmp;
+
+		}
+
+		data[num_elem++] = new_element; //post increment, al passar la linea és quan s'incrementa
 
 	}
 
@@ -43,16 +62,19 @@ public:
 		return capacity;
 	}
 
-	uint At() const{
+	uint Size() const{
 		return num_elem;
 	}
 
-	tmp c_str() const{
-
-		return data;
+	TYPE& At(const uint& position)const{
+	
+		if (position > capacity){
+			printf("Out of capacity");
+		}
+		return data[position];
 	}
 
-	bool empty(){
+	bool empty() const{
 		
 		return (num_elem == 0);
 		
@@ -69,7 +91,7 @@ public:
 			delete[] data;
 			capacity += new_array.capacity;
 			num_elem += new_array.num_elem;
-			data = new tmp[capacity];
+			data = new TYPE[capacity];
 		}
 		strcat_s(data, capacity, new_array.data);
 
@@ -80,15 +102,15 @@ public:
 		if (capacity < num_elem + new_array.num_elem){
 			delete[] data;
 			capacity += new_array.capacity;
-			data = new tmp[capacity];
+			data = new TYPE[capacity];
 		}
 		else if (new_array.capacity < num_elem + new_array.num_elem){
 			delete[] new_array.data;
 			new_array.capacity += capacity;
-			new_array.data = new tmp[new_array.capacity];
+			new_array.data = new TYPE[new_array.capacity];
 		}
 		strcat_s(new_array.data, capacity, data);
-		memcpy(data, new_array.data, new_array.num_elem*sizeof(tmp));
+		memcpy(data, new_array.data, new_array.num_elem*sizeof(TYPE));
 
 		return *this;
 	}
